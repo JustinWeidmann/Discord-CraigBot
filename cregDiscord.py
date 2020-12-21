df7fde28eedd279665430303dfba1e18d7fd4c6d
Justin Weidmann <justinweidmann2@gmail.com>
2020-12-21 11:57:17 -0700

    initial commit

---
+++ cregDiscord.py
@@ -0,0 +1,113 @@
+import discord
+import scrape
+import asyncio
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+TOKEN = "Nzg1NjkzNDk3MTU5OTc0OTgy.X87kFw.KGnloEjq4qVHfUw_Z1qmzQelgac"
+searchTerm = "metal"
+channelID = 518298731855216642
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    #message.channel.send(f'Heyyyy mann im your local scraper for {searchTerm} and posting to {channelID}. !creg help to get started bro ͡° ͜ʖ ͡ –')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('!creg help'):
+        await message.channel.send(f'**!setChannelID <channelID>**:   Sets defalt channel\n**!give me the scoop**:   Prints all results for {searchTerm}\n**!search <term>**:   Searches for <term> then prints all results')
+
+    # if message.content.startswith('!setChannelID'):
+    #     global channelID
+    #     channelID = message.content[14:]
+    #     await message.channel.send(f'Channel set to: **{channelID}**')
+
+    if message.content.startswith('!give me the scoop'):
+        await message.channel.send("Give me a sec...")
+        global oldPostTime
+
+        cregResults = scrape.runScrape(searchTerm)
+        postIndex = cregResults[0]
+        postTitle = cregResults[1]
+        postTime = cregResults[2]
+        postLocation = cregResults[3]
+        postURL = cregResults[4]
+
+        i = 0
+        for cregResults in postIndex:
+            await message.channel.send(f'{postIndex[i]}: {postTitle[i]}\n {postTime[i]}\n {postLocation[i]}\n {postURL[i]}\n')
+
+            i += 1
+        
+        oldPostTime = postTime
+
+    if message.content.startswith('!search'):
+        searchGrab = message.content[8:]
+        await message.channel.send(f'Searching for {searchGrab}...')
+        print(f'Scraping for {searchGrab}...')
+
+        cregResults = scrape.runScrape(searchGrab)
+        postIndex = cregResults[0]
+        postTitle = cregResults[1]
+        postTime = cregResults[2]
+        postLocation = cregResults[3]
+        postURL = cregResults[4]
+
+        i = 0
+        for cregResults in postIndex:
+            await message.channel.send(f'{postIndex[i]}: {postTitle[i]}\n {postTime[i]}\n {postLocation[i]}\n {postURL[i]}\n')
+            i += 1
+
+    # if message.content.startswith('!stop'):
+    #     print("got stop")
+    #     break command
+
+def initialCheck():
+    print("Runing ititial check...")
+    cregResults = scrape.runScrape(searchTerm)
+    global oldPostTime
+    oldPostTime = cregResults[2]
+
+async def checkForNew():
+    global oldPostTime
+    global channelID
+    channel = client.get_channel(channelID)    # Discord Channel ID, Room-1:528448098293514240,
+    print("Checking for new posts...")
+    cregResults = scrape.runScrape(searchTerm)
+    postTitle = cregResults[1]
+    postTime = cregResults[2]
+    postLocation = cregResults[3]
+    postURL = cregResults[4]
+
+    
+    i = 0
+    for cregResults in postTime:
+        print(f'Checking: {i}')
+        if postTime[i] <= oldPostTime[0]:
+            print("Ran out of new")
+            print(f'Because: {postTime[i]} is <= then {oldPostTime[0]}')
+            #await channel.send("Nada")
+            break
+        elif postTime[i] > oldPostTime[0]:
+            print(f'{i} is new!!')
+            print(f'Because: {postTime[i]} is > then {oldPostTime[0]}')
+            await channel.send(f'{postTitle[i]}\n {postTime[i]}\n {postLocation[i]} {postURL[i]}\n')
+        
+        i += 1
+    
+    oldPostTime = postTime
+    
+
+initialCheck()
+if __name__ == '__main__':
+    sched = AsyncIOScheduler()
+    sched.add_job(checkForNew, 'interval', minutes = 10)
+    sched.start()
+client.run(TOKEN)
---
+++ scrape.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import requests
+
+def runScrape(QUERY):
+    BASE_URL = 'https://denver.craigslist.org'
+        
+    def stepThroughPages(posts, pageLink):  #Recursion function - Grabs all page data
+        source = requests.get(BASE_URL + pageLink).text
+        soup = BeautifulSoup(source, 'html.parser')
+        nextButton = soup.find('a', class_='button next')
+        posts.extend(soup.find_all('li', 'result-row'))
+
+
+        if nextButton is None: return posts
+        return stepThroughPages(posts, nextButton.get('href'))
+
+    def outputResults(posts):
+        resultIndex = []
+        resultTitle = []
+        resultTime = []
+        resultLocation = []
+        resultURL = []
+        for i, post in enumerate(posts):
+            postTitle = post.find('a', class_='result-title').get_text()
+            postTime = post.find('time', class_='result-date').get('datetime')
+            postPreLocation = post.find('span', class_='result-hood')
+            if postPreLocation is None:
+                postLocation = "(IDK bro ¯\_(ツ)_/¯)"
+            else:
+                postLocation = postPreLocation.get_text()
+            postURL = post.find('a', class_='result-title').get('href')
+
+            #print(f'{i}: {postTitle}\n {postTime}\n {postLocation}\n {postURL}\n')
+            
+            resultIndex.append(i)
+            resultTitle.append(postTitle)
+            resultTime.append(postTime)
+            resultLocation.append(postLocation)
+            resultURL.append(postURL)
+        return resultIndex, resultTitle, resultTime, resultLocation, resultURL
+            
+
+    totalPosts = stepThroughPages([], '/search/zip')
+
+    totalQueryPosts = [post for post in totalPosts if QUERY in post.find('a', class_='result-title').get_text().lower()]    #List comprehension - Sorts for metal(or whatever you want)
+    print(f'{len(totalQueryPosts)} results containing "{QUERY}"')
+
+    scrapeResults = outputResults(totalQueryPosts)
+
+    postPercentage = round((len(totalQueryPosts) / len(totalPosts) * 100), 1)
+    print(f'{len(totalQueryPosts)} of the total {len(totalPosts)} free posts contained {QUERY}. Thats {postPercentage}%!')
+
+    return scrapeResults
+
+#cregResults = runScrape("metal")
+#print(cregResults)
